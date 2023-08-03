32dfa47671fe0613f8a3b48a7259f0c2a673e737
aimakhotka <all.mahotka2012@yandex.ru>
2023-08-03 11:05:51 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='pylint-hook',
     version='1.0.0',
     description='Pylint git hook with score',
-    author='Alla Makhotka'
-    author_email='all.mahotka2012@yandex.ru'
+    author='Alla Makhotka',
+    author_email='all.mahotka2012@yandex.ru',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
